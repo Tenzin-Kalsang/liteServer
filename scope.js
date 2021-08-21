2e11f70c181255bedd389808e977347fc5df61e7
Tenzin-Kalsang <tznklsng28@gmail.com>
2021-08-20 21:21:09 -0400

    first commit

---
+++ scope.js
@@ -0,0 +1,11 @@
+const myVar = 2;
+function testVar() {
+  const myVar = 1;
+    console.log('Inside the function, myVar is: ' + myVar);
+}
+testVar();
+
+function test() {
+   let bell = "hi";
+   document.write("bell");
+}
